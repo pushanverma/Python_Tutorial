b9aa960aad13ae4787e6a0396ab9b786c02b978e
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2026-08-29 19:57:03 -0400

    adding Comments and variable code

---
+++ 2_Comments_Variables.py
@@ -0,0 +1,23 @@
+#--------------------------- 2.Comments ----------------------------------------
+
+# 2.1 Using ( Shift + / )
+# Hello this is Pushan verma 
+# i am a student at Purdue University 
+# I am currently Learning -> Python , Machine Learning and Deep Learning 
+
+
+
+# 2.2 DocString ->Enclosing within the three double Quotes / Single Quotes 
+
+""" hello this is pushan verma and I am from India 
+I am currently studying Computer science from Purdue University 
+I will be studying Python , Machine Learning and Deep Learning """
+
+''' Hello this is Pushan Verma  '''
+
+# -------------3.Variables and Different type of Variable Casing--------------------
+
+PushanVerma = "Hello this is Pushan Verma";     # PasCal Case 
+pushanVerma = "Hello this is Pushan Verma";     # Camel Case 
+pushan_verma ="Hello this is Pushan Verma";     # Snake Case 
+
