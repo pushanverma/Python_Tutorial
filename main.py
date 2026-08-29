697172db1d9363939cecc8509537ee49bb6977b1
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2026-08-29 15:49:04 -0400

    How to comment in python code added

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Hello world");
 
+#1.  Hello World Program 
+
+print("Hello world this is Pushan verma ")
+
+
+#2. Comments 
+
+# 2.1 Using ( Shift + / )
+# Hello this is Pushan verma 
+# i am a student at Purdue Fort wayne 
+# I am currently Learning -> Python , Machine Learning and Deep Learning 
+
+
+
+# 2.2 Enclosing within the three double Quotes / Single Quotes 
+
+""" hello this is pushan verma and I am from India 
+I am currently studying Computer science from Purdue University 
+I will be studying Python , Machine Learning and Deep Learning """
+
+''' Hello this is Pushan Verma  '''
